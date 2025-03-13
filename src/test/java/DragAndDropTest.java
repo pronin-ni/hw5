8778d72671f3e4384fbe9378cfd17590b1b3d91d
npronin <noxywille@yandex.ru>
2025-03-13 08:33:48 +0300

    DragAndDropTest update

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,19 +12,30 @@ public class DragAndDropTest {
     static void confLoad() {
         Configuration.pageLoadStrategy = "eager";
     }
-    @Test
-    void DragSquaresTest(){
+
+    @BeforeEach
+    void openForm(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
+    }
+
+    @Test
+    void dragSquaresWithActionsTest(){
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        actions().moveToElement($("#column-a")).clickAndHold()
+                .moveToElement($("#column-b"))
+                .release()
+                .perform();
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
-        // Вернем все обратно на свои места
-        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+    }
+    @Test
+    void dragSquaresWithDragAndDropTest(){
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 
 }
